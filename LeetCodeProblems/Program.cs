7cc6635d311a3161a3998c11db935db7f4205ee4
George Kalashnikov <george.kalashnikov@kaspersky.com>
2021-08-03 23:38:56 +0300

    Removed old tests from Program.cs

--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -9,14 +9,6 @@ namespace LeetCodeProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            // LeetCodeChallenges.RemoveDuplicates.RemoveDuplicatesTest();
-            // LeetCodeChallenges.QueensAttackingTheKingTests.RunTests();
-            // LeetCodeChallenges.ThreeSumClosestTests.RunTests();
-            // LeetCodeChallenges.IntegerToRomanTests.RunTests();
-            // LeetCodeChallenges.WordSearchTests.Test();
-            // LeetCodeChallenges.SumOfTheRootToLeafBinaryTests.Test();
-            // LeetCodeChallenges.BinaryTreeInorderTraversalTests.Test();
-            // LeetCodeChallenges.PerfectSquaresTests.TrivialSamplesShouldWork();
         }
     }
 }
