55d62ef23383e221eae5137a3090425337124154
savamura <georgekalasjhnikov@gmail.com>
2021-09-05 11:29:07 +0300

    KLargest array elements

--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using LeetCodeChallenges.DataStructures;
 
-namespace LeetCodeProblems
+namespace LeetCodeChallenges
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             // LeetCodeChallenges.RemoveDuplicates.RemoveDuplicatesTest();
             // LeetCodeChallenges.QueensAttackingTheKingTests.RunTests();
             // LeetCodeChallenges.ThreeSumClosestTests.RunTests();
@@ -17,6 +16,34 @@ namespace LeetCodeProblems
             // LeetCodeChallenges.SumOfTheRootToLeafBinaryTests.Test();
             // LeetCodeChallenges.BinaryTreeInorderTraversalTests.Test();
             // LeetCodeChallenges.PerfectSquaresTests.TrivialSamplesShouldWork();
+
+            var soluton = new KLargestArrayElements();
+            //Func<int[]> assign = () => { return new int[] {13, 4, 1, 5, 6, 77, 9, 9, 12, 19, 53, 0}; };
+            //Func<int[]> assign = () => { return new int[] {3,2,3,1,2,4,5,5,6}; };
+            Func<int[]> assign = () => { return new int[] {21, 20, 21}; };
+            //Func<int[]> assign = () => { return new int[] {-1, 2, 0}; };
+            var nums = assign();
+            
+            // {3,2,3,1,2,4,5,5,6};//{ 13, 4, 1, 5, 6, 77, 9, 9, 12, 19, 53, 0 };// { -1, 2, 0 };
+
+            for (var k = 0; k < nums.Length; k++)
+            {
+                nums = assign();
+                var result = soluton.QuickSelect(nums, k);
+                PrintArray(nums);
+                Console.WriteLine($"{k}th biggest is {result} ");
+                Console.ReadLine();
+            }
+        }
+
+        static void PrintArray(int[] nums)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
+            
+            Console.WriteLine();
         }
     }
 }
